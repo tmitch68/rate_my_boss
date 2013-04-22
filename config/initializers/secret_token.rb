aefef3c54591b10d4f0d2ab213ace6022d6eb492
Tim <tmitch@cs.unm.edu>
2013-04-21 18:35:51 -0600

    Added a Pages controller

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RateMyBoss::Application.config.secret_token = '7b7428c18abead53e09e10a10b387f63b316a69006b8e531302551ee82e796a3a0dc4bc70fa65e633b60fdea086820c42ca20e67fdcb734c229aaf9771da2de6'
